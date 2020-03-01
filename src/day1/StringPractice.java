281228ca0f4b3262e9cee61d170144d2b9ee64f8
Shahadet <susha384@gmail.com>
2020-02-29 21:35:21 -0600

    new massege

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -3,6 +3,6 @@ package day1;
 public class StringPractice {
     public static void main(String[] args) {
         System.out.println("hello World");
-
+        System.out.println("Hello batch 15");
     }
 }
